a8a5dfdbde061e5d7ad533197673e7dbe4cb2170
lavsora <ya.karpich@gmail.com>
2022-12-05 16:34:22 +0300

    delete import from chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,7 +2,6 @@ import { useContext, useState } from 'react';
 import { Context } from "../index";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Avatar, Button, Container, Grid } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Loader from "./Loader";
 import firebase from "firebase";
