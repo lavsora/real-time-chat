3a8a8982a889dbb662de436c80e828212a976c9d
lavsora <ya.karpich@gmail.com>
2022-12-12 17:12:05 +0300

    refactor

--- src/utils/useValidation.js
+++ src/utils/useValidation.js
@@ -24,25 +24,6 @@ const useValidation = (value, validations) => {
             }
         }
 
-        return () => {
-            for (const validation in validations) {
-                switch (validation) {
-                    case 'minLength':
-                        value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
-                        break;
-                    case 'isEmpty':
-                        value ? setEmpty(false) : setEmpty(true)
-                        break;
-                    case 'emailError':
-                        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                        re.test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true)
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
         // eslint-disable-next-line
     }, [value])
 
